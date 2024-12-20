1c087c084c7473fbc03994eb3eb5d14fa672222f
overtrue <anzhengchao@gmail.com>
2024-12-20 09:56:55 +0800

    feat: benchmark

---
+++ benchmark/test.php
@@ -0,0 +1,65 @@
+<?php
+
+include __DIR__ .'/../vendor/autoload.php';
+
+use Overtrue\PHPOpenCC\OpenCC;
+use Overtrue\PHPOpenCC\Dictionary;
+use function Termwind\{render};
+
+$chunkSize = $argv[1] ?? 10;
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+
+$chunks = str_split($input, $chunkSize);
+
+$dictionaries = array_keys(Dictionary::SETS_MAP);
+
+$output = [];
+
+foreach ($dictionaries as $strategy) {
+  echo "Testing with strategy: {$strategy}...";
+
+  $start = microtime(true);
+
+  foreach ($chunks as $chunk) {
+    OpenCC::convert($chunk, $strategy);
+  }
+
+  $usage = round(microtime(true) - $start, 5) * 1000;
+  $avgUsagePerChunk = $usage / count($chunks);
+  $avgUsagePerChar = $usage / mb_strlen($input);
+  $html[] = "<tr>
+                <td><span class=\"text-teal-500\">{$strategy}</span></td>
+                <td><span class=\"text-green-500\">{$usage} ms</span></td>
+                <td><span class=\"text-green-500\">{$avgUsagePerChunk} ms</span></td>
+                <td><span class=\"text-green-500\">{$avgUsagePerChar} ms</span></td>
+             </tr>
+        ";
+  echo "Done.\n";
+}
+
+$html = implode("\n", $html);
+$chunksCount = count($chunks);
+$textLength = mb_strlen($input);
+
+render(<<<"HTML"
+    <div class="m-2">
+        <div class="px-1 bg-green-600 text-white">PHP OpenCC</div>
+
+        <div class="py-1">
+            Converted <span class="text-teal-500">{$textLength}</span> chars(<span class="text-teal-500">{$chunksCount}</span> chunks/{$chunkSize} chars per chunk) with following strategies:
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Strategy</th>
+                    <th>Time Usage</th>
+                    <th>Avg Usage Per Chunk ({$chunkSize} chars)</th>
+                    <th>Avg Usage Per Char</th>
+                </tr>
+            </thead>
+            {$html}
+        </table>
+    </div>
+HTML);
